6ac698fa7cd960656f33d80f640655667d38b9e9
Prashant Pal <92903544+Prashant9420@users.noreply.github.com>
2023-08-05 23:34:11 +0530

    Update server_url.js

--- src/server_url.js
+++ src/server_url.js
@@ -1,2 +1,3 @@
-const SERVER_URL = "http://localhost:5000";
+// const SERVER_URL = "http://localhost:5000";
+const SERVER_URL = "https://formbuilder-server.onrender.com";
 export default SERVER_URL;
